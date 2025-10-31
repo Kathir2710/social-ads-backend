6f9b3aba209442f55806e2ff0daf27c1ba6d8b79
cybertron-syed <sdsyed46@gmail.com>
2025-10-31 17:47:49 +0530

    Initial commit - front554

--- server.js
+++ server.js
@@ -11,7 +11,15 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://data-add-management.netlify.app",  // your Netlify frontend
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
